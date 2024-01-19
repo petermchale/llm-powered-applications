ca6c9c4840412dcfbe097f8590ca8ac11aca15cd
petermchale <peter.thomas.mchale@gmail.com>
2024-01-19 11:07:01 -0700

    given a disease-relevant gene, download all articles associated with it

--- STR-chatbot/download-docs.sh
+++ STR-chatbot/download-docs.sh
@@ -4,8 +4,22 @@ set -o pipefail
 set -o xtrace
 set -o nounset 
 
-while read id; do
-  curl \
-    "https://www.ncbi.nlm.nih.gov/books/${id}/pdf/Bookshelf_${id}.pdf" \
-    --output "docs/${id}.pdf"
-done < GeneReviewsIDs.txt
+# while read id; do
+#   curl \
+#     "https://www.ncbi.nlm.nih.gov/books/${id}/pdf/Bookshelf_${id}.pdf" \
+#     --output "docs/${id}.pdf"
+# done < GeneReviewsIDs.txt
+
+# https://github.com/laurelhiatt/STRchive/tree/lit/data
+for gene in "AFF2"; do 
+  mkdir -p "docs/${gene}"
+  curl "https://raw.githubusercontent.com/laurelhiatt/STRchive/lit/data/${gene}01.txt" \
+    | grep "PMID-" \
+    | awk '{ print $2 }' \
+  > "docs/${gene}/PMIDs.txt"
+  while read id; do
+    curl \
+      "https://www.ncbi.nlm.nih.gov/research/bionlp/RESTful/pmcoa.cgi/BioC_xml/${id}/unicode" \
+      --output "docs/${gene}/${id}.xml"
+  done < "docs/${gene}/PMIDs.txt"
+done 
